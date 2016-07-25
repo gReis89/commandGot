649df55b65b8bbdde4c40681f11b94a0a7fa7699
zfael <rafael.p.bertelli@gmail.com>
2016-07-25 16:00:14 -0300

    Adjustment when a character has been updated, aiming return the correct object

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,23 +1,23 @@
 var character = require('../models/characters');
 
 /* READ ALL CHARACTERS */
-exports.list = function(callback) {
-    character.find(function(err,characters){
+exports.list = function (callback) {
+    character.find(function (err, characters) {
         callback(characters);
     });
 };
 
 /* READ A SPECIFIC CHARACTER */
-exports.listOne = function(id, callback) {
+exports.listOne = function (id, callback) {
 
     //quantifying the visits
-    character.update({_id: id}, {$inc: {visits: 1}}, function(err, char) {
-        if(err) 
+    character.update({ _id: id }, { $inc: { visits: 1 } }, function (err, char) {
+        if (err)
             throw err;
 
         //finding the character
-        character.findOne({_id: id}, function(err, char) {
-            if (err) 
+        character.findOne({ _id: id }, function (err, char) {
+            if (err)
                 throw err;
 
             callback(char);
@@ -26,40 +26,48 @@ exports.listOne = function(id, callback) {
 };
 
 /* ADD ONE LIKE TO A UNIQUE CHARACTER BY ID */
-exports.like = function(id, callback) {
+exports.like = function (id, callback) {
 
-    character.findByIdAndUpdate(id, {$inc: {likes:1}}, function(err, char) {
-      if (err) 
-        throw err;
+    character.update({ _id: id }, { $inc: { likes: 1 } }, function (err, char) {
 
-      callback(char);
+        if (err)
+            throw err;
+
+        //finding the character
+        character.findOne({ _id: id }, function (err, char) {
+            if (err)
+                throw err;
+            
+            
+            callback(char);
+        });
     });
 };
 
 /* INSERT A NEW CHARACTER */
-exports.add = function(path, name, about, callback) {
+exports.add = function (path, name, about, callback) {
 
     var newCharacter = {
-      path : path,
-      name : name,
-      about: about,
-      likes: 0,
-      visits : 0
+        path: path,
+        name: name,
+        about: about,
+        likes: 0,
+        visits: 0
     };
 
     var prepare_and = new character(newCharacter);
     prepare_and.save(function (err, data) {
-      if (err) 
-        throw err;
-      else 
-        callback(data);
+        if (err)
+            throw err;
+        else
+            callback(data);
     });
 
 };
 
 /* REMOVE A CHARACTER BY ID */
-exports.delete = function(id, callback) {
-    character.remove({_id: id}, function(err,removed) {
+exports.delete = function (id, callback) {
+    character.remove({ _id: id }, function (err, removed) {
         if (err)
             throw err;
         else
@@ -68,10 +76,19 @@ exports.delete = function(id, callback) {
 };
 
 /* UPDATE A CHAR  ACTER DATA */
-exports.update = function(body, callback) {
-    character.findByIdAndUpdate(body.id, {name: body.char_name, about: body.about, path: body.path}, function(err, char) {
-      if (err) 
-        throw err;
-      callback(char);
+exports.update = function (body, callback) {
+
+    character.update(body.id, {name: body.char_name, about: body.about, path: body.path}, function (err, char) {
+
+        if (err)
+            throw err;
+
+        //finding the character
+        character.findOne({ _id: body.id }, function (err, char) {
+            if (err)
+                throw err;
+
+            callback(char);
+        });
     });
 };
