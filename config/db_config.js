3eb7459be424f0003156e5d5d1fa5a7b6d9457c3
zfael <rafael.p.bertelli@gmail.com>
2016-07-25 15:57:49 -0300

    Add a URI to a mongoDB test

--- config/db_config.js
+++ config/db_config.js
@@ -1,13 +1,27 @@
+var env = process.env.NODE_ENV || 'development';
 var mongoose = require('mongoose');
 
-//var urlString = 'mongodb://localhost/API';
-var urlString = 'mongodb://command:102030@ds027145.mlab.com:27145/game_of_thrones';
+var config = {
+    development: {
+        db: 'mongodb://command:102030@ds027145.mlab.com:27145/game_of_thrones'
+    },
+    production: {
+        db: 'mongodb://command:102030@ds027145.mlab.com:27145/game_of_thrones'
+    },
+    test: {
+        db: 'mongodb://localhost/node-test'
+    }
+};
 
-mongoose.connect(urlString, function(error, res) {
+//var urlString = 'mongodb://localhost/API';
+var urlString = config[env].db;
+mongoose.connect(urlString, function (error, res) {
     if (error) {
         console.log(error);
         console.log('But you forgot your coat =/');
     } else {
-        console.log('And you are connected!');
+        console.log('And you are connected! [' + urlString + ']');
     }
 });
+
+//module.exports = config[env];
